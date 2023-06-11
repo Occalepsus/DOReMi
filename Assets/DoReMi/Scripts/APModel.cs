4b9b26095f0cbae976c411e040682ec554091e08
Occalepsus <juju.joly@free.fr>
2023-06-11 20:42:15 +0200

    update(APModel): added method to match model with user data input or with measure. Changed ap object position to floor, under the hand, instead of the hand.

--- Assets/DoReMi/Scripts/APModel.cs
+++ Assets/DoReMi/Scripts/APModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,17 +18,15 @@ namespace Assets.DoReMi.Scripts
         public Transform anchor;
 
         /// <summary>
-        /// The parameters of the formula
-        /// </summary>
-        public int powerEmitterDBm;
-        public float gainEmitterDBi;
-        public float gainReceiverDBi;
-        public float wifiFrequencyHz;
-
-        /// <summary>
         /// The const part value of the formula
         /// </summary>
-        private float _constValue;
+        private float _constValue = float.MinValue;
+
+        // TODO: Replace this with user values input
+        private void Awake()
+        {
+            MatchWithUserValues(0, 0, 0, 2.4e9f);
+        }
 
         /// <summary>
         /// Gets the computed signal strength at pos
@@ -41,17 +40,46 @@ namespace Assets.DoReMi.Scripts
             return _constValue - (20 * Mathf.Log10(dist));
         }
 
-        private void Awake()
+        /// <summary>
+        /// Places the APModel at the given position
+        /// </summary>
+        /// <param name="newTransfrom">The position to give</param>
+        /// <exception cref="InvalidOperationException">The component has not been initialized</exception>
+        public void PlaceAPModel(Transform newTransfrom)
         {
-            // Computing the const part of the Friis formula in our model
-            _constValue = powerEmitterDBm + gainEmitterDBi + gainReceiverDBi + (20 * Mathf.Log10((float)(3e8 / (4 * Mathf.PI * wifiFrequencyHz))));
+            if (_constValue == float.MinValue)
+            {
+                Debug.LogError("Error: APModel not initialized");
+                throw new InvalidOperationException("APModel not initialized. Please call MatchWithLevel or MatchWithUserValues before placing the APModel");
+            }
+            transform.position = new(newTransfrom.position.x, 0, newTransfrom.position.z);
+            gridManager.ComputeGrid(GetSignalStrengthAt);
+        }
+
+        /// <summary>
+        /// Matches the APModel at origin with the given value
+        /// </summary>
+        /// <param name="valueToMatch"></param>
+        public void MatchWithLevel(int valueToMatch)
+        {
+            // Setting the const part of the Friis formula in our model with the value to match
+            _constValue = valueToMatch;
             // Sets the caller of the method of GridManager that gets the computed signal strength
             gridManager.ComputeGrid(GetSignalStrengthAt);
         }
 
-        public void PlaceAPModel(Transform newTransfrom)
+        /// <summary>
+        /// Matches the APModel at origin with the given values
+        /// </summary>
+        /// <param name="powerEmitterDBm">The power of the emitter in dBm</param>
+        /// <param name="gainEmitterDBi">The gain of the emitter in dBi</param>
+        /// <param name="gainReceiverDBi">The gain of the receiver in dBi</param>
+        /// <param name="wifiFrequencyHz">The frequency of the WiFi in Hz (2.4e9 or 5.0e9)</param>
+        public void MatchWithUserValues(int powerEmitterDBm, float gainEmitterDBi, float gainReceiverDBi, float wifiFrequencyHz)
         {
-            transform.position = newTransfrom.position;
+            // Computing the const part of the Friis formula in our model
+            _constValue = powerEmitterDBm + gainEmitterDBi + gainReceiverDBi + (20 * Mathf.Log10((float)(3e8 / (4 * Mathf.PI * wifiFrequencyHz))));
+            // Sets the caller of the method of GridManager that gets the computed signal strength
             gridManager.ComputeGrid(GetSignalStrengthAt);
         }
     }
